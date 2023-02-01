911c620ee25cfb840072e5a00d04877ed04182b6
singhrohit81111 <singhrohit81111@gmail.com>
2023-02-01 16:46:25 +0530

    11th commit

--- src/Launch/Routtes.jsx
+++ src/Launch/Routtes.jsx
@@ -3,20 +3,20 @@ import Launches from '../Launch/Launches';
 import Upcoming from '../Launch/Upcoming';
 import Past from '../Launch/Past';
 import Home from '../components/Home';
-import { HashRouter , Route ,Routes } from 'react-router-dom'
+import { BrowserBrowserRouter, Router , Route ,Routes } from 'react-router-dom'
 
 export default function 
 () {
   return (
-    <HashRouter >
+    <BrowserRouter >
     <Routes>
-        <Route path="/spacexLaunch">
+        <Route path="/">
             <Route index element={<Home/>}/>
-            <Route path="/launches" element={<Launches/>}/>
-            <Route path="/upcoming" element={<Upcoming/>}/>
-            <Route path="/past" element={<Past/>}/>
+            <Route path="launches" element={<Launches/>}/>
+            <Route path="upcoming" element={<Upcoming/>}/>
+            <Route path="past" element={<Past/>}/>
         </Route>
     </Routes>
-    </HashRouter >
+    </BrowserRouter >
   )
 }
